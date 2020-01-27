984b46dbf1c471352bf600541d3381010bb9f227
JustinBurrows <justinburrows.software@gmail.com>
2020-01-27 15:49:54 -0800

    Choose your own adventure

---
+++ game.js
@@ -0,0 +1,39 @@
+alert("Welcome to Unruly Evening where you decide your fate...but do you? muh hahahaha *deep scary voice*");
+
+
+let decision1 = prompt("Your deciding who you want take with you on a trip, you can: A) take one of your day one homies B) take the hot chick that works at hooters  C) go with your mom");
+let a = "a";
+let b = "b";
+let c = "c";
+
+
+
+if (decision1 == a) {
+    alert("Your day one homie is down, but you gotta pay everything for him cause he's looking for a job ");
+}
+
+if (decision1 == b) {
+    alert("the hot chick is down but she going to bring a friend with her and you got to pay for them both");
+}
+
+if (decision1 == c) {
+    alert("you mom is down, and she so excited she'd going to pay for everthing that way she can use alll your money when she gets to the destination ");
+}
+
+
+let decision2 = prompt("You decide youve been working superhard so know you want to take some time to your self, you can: A) go to france B) go to a bar or C) go to Iceland");
+
+
+
+
+if (decision2 == a) {
+    alert("You now partying on a roof top in France with a infinity pool, 40 supermodels, all of your good friends, Dom Kennedy, 40 gallon keg, foam pit, a zebra, then Kobe is resurrected and joins you in a game of beer pong against Paul pierce & JR smith, and you and Kobe win by  1 cup, then end.");
+}
+
+if (decision2 == b) {
+    alert("You end up at a party, where the back street boys are performing, there are 3 people there other than you, the person whose with the person you want to date, your math teacher from middle school, and the tortilla lady, the end.");
+}
+
+if (decision2 == c) {
+    alert("You wake up in ice land on public transit and you can’t remember anything, the only thing you have in your pocket is a note that says “gotcha loser” and your whole body is sore, your glasses and broken and a tooth is missing.");
+}
