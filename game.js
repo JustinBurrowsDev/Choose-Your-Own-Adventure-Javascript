d66ae5170803382d657ad9af194dea92b4a664f1
JustinBurrowsDev <58059462+JustinBurrowsDev@users.noreply.github.com>
2020-02-22 14:42:23 -0800

    Update game.js

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 alert("Welcome to Unruly Evening where you decide your fate...but do you? muh hahahaha *deep scary voice*");
 
 
-let decision1 = prompt("Your deciding who you want take with you on a trip, you can: A) take one of your day one homies B) take the hot chick that works at hooters  C) go with your mom");
+let decision1 = prompt("Your deciding who you want take with you on a trip, you can: A) take one of your day one homies B) take the pretty girl from hooters  C) go with your mom");
 let a = "a";
 let b = "b";
 let c = "c";
@@ -9,11 +9,11 @@ let c = "c";
 
 
 if (decision1 == a) {
-    alert("Your day one homie is down, but you gotta pay everything for him cause he's looking for a job ");
+    alert("Your day one homie is down, but you got to pay everything for him cause he's looking for a job ");
 }
 
 if (decision1 == b) {
-    alert("the hot chick is down but she going to bring a friend with her and you got to pay for them both");
+    alert("the pretty girl is down but she going to bring a friend with her and you got to pay for them both");
 }
 
 if (decision1 == c) {
@@ -27,7 +27,7 @@ let decision2 = prompt("You decide youve been working superhard so know you want
 
 
 if (decision2 == a) {
-    alert("You now partying on a roof top in France with a infinity pool, 40 supermodels, all of your good friends, Dom Kennedy, 40 gallon keg, foam pit, a zebra, then Kobe is resurrected and joins you in a game of beer pong against Paul pierce & JR smith, and you and Kobe win by  1 cup, then end.");
+    alert("You now partying on a roof top in France with a infinity pool, all of your good friends, Dom Kennedy, foam pit, a zebra, then Kobe is resurrected and joins you in a game of beer pong against Paul pierce & JR smith, and you and Kobe win by  1 cup, then end.");
 }
 
 if (decision2 == b) {
@@ -35,5 +35,5 @@ if (decision2 == b) {
 }
 
 if (decision2 == c) {
-    alert("You wake up in ice land on public transit and you can’t remember anything, the only thing you have in your pocket is a note that says “gotcha loser” and your whole body is sore, your glasses and broken and a tooth is missing.");
+    alert("You wake up in ice land on public transit and you can’t remember anything, the only thing you have in your pocket is a note that says “gotcha loser” and your whole body is sore, your glasses are broken and a tooth is missing.");
 }
